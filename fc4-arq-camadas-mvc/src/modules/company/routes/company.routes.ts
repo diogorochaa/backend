5ff6063d2972b6260f157a5987791efa49b0921a
Diogo Rocha <diogorocha12@live.com>
2026-07-08 10:44:40 -0300

    chore: add .gitignore to exclude node_modules and package-lock.json

--- fc4-arq-camadas-mvc/src/modules/company/routes/company.routes.ts
+++ fc4-arq-camadas-mvc/src/modules/company/routes/company.routes.ts
@@ -9,11 +9,7 @@ const companyRepository = new InMemoryCompanyRepository();
 const companyService = new CompanyServiceImpl(companyRepository);
 const companyController = new CompanyController(companyService);
 
-router.post('/companies/', (
-    req,
-    res,
-    next) =>
-    companyController.create(req, res, next));
+router.post('/companies/', (req, res, next) => companyController.create(req, res, next));
 router.get('/companies/', (req, res, next) => companyController.findAll(req, res, next));
 router.get('/companies/:id', (req, res, next) => companyController.findByID(req, res, next));
 router.get('/companies/cnpj/:cnpj', (req, res, next) => companyController.findByCNPJ(req, res, next));
